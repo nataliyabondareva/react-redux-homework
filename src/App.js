ff220b6600f23ad53a995caa4f87f8db27873ac6
tallybee <talbondareva@gmail.com>
2019-05-11 09:02:06 +0200

    'added render'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+
+export default class DropdownMenu extends React.Component {
+  state = []
+  render() {
+    return (
+      <div className="choose-car">
+        <form>
+          <label>
+            Car:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+const data = {
+  "Ivel Z3": {
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  "Bally Astrocade": {
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  "Sord M200 Smart Home Computer": {
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  "Commodore 64": {
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+}
+
+// export default App;
